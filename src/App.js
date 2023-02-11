8f8a94f933b57875edfc29fb4e27a7ed4fb743fb
tahabisginsoftware <tahabisginsoftware@gmail.com>
2023-02-11 14:33:34 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,21 +1,36 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
+import logo from './logo512.png';
 import './App.css';
 
+import names from 'random-names-generator';
+
 function App() {
+
+  const [name, setName] = useState('');
+
+  useEffect(() => {
+    // Generate
+    setName(names.random());
+  }, []);
+  
+  const handleClick = () => {
+    setName(names.random());
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={logo} className="App-logo" alt="logo" />  
+          <h2>Click/Reload for new Name.</h2>
+          <button className='getButton' onClick={handleClick}>Get Random Name</button>
+          <h2>{name}</h2>        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://tahabisginsoftware.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Made by Taha Bisgin.
         </a>
       </header>
     </div>
